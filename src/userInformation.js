739926863c8570a7a2d7d49ad47316b79b5e61e8
verobega <pooh_tres@hotmail.com>
2020-04-22 08:11:27 -0500

    codeRoot

--- src/userInformation.js
+++ src/userInformation.js
@@ -1,7 +1,7 @@
 //POST
 
 let addBtn = document.querySelector('#myCommit')
-let text = document.querySelector('#showComment')
+let text = document.querySelector('#showComment')//variable con id en donde se pintaran los post, textArea 
 let fileInput = document.querySelector('#file') //variable para la prueba de subir imagen
 
 //global
@@ -14,7 +14,7 @@ fileInput.onchange = e => {
     let file = e.target.files[0]  
     firebase.storage().ref("memes").child(file.name).put(file)
         .then(snap => {   //¿Donde esta el archivo? En file.name
-            return snap.ref.getDownloadURL()
+            return snap.ref.getDownloadURL() //conseguir el link de la imagen. Retornas la promesa y concatenas el otro then
         })
         .then(link => {
             url = link
@@ -24,23 +24,25 @@ fileInput.onchange = e => {
         })
 }
 
+
+//traer la informacion del post cuando se le da clic en el boton
 addBtn.onclick = event => {
     // traer el texto
     let post = {
         body: text.value,
         user: "spiderman",
         date: new Date(),
-        img: url
+        img: url //variable global, aqui se almacena la imagen cuando ya se tiene el link que envio la funcion onchange
     }
     //  [ASINCRONO] 
 
     addNewPost(post)
         // ESto es asíncrono
-        .then(res => {
-            console.log(res)
+        .then(res => { //esto es la promesa
+            console.log(res) //este es el resultado de la promesa
         })
         .catch(err => {
-            console.log("todo valió baby: ", err)
+            console.log("todo valió baby: ", err) //esto es el error cuando la respuesta es negativa
         })
 }
 
@@ -49,17 +51,20 @@ addBtn.onclick = event => {
 let db = firebase.firestore() // YOLO
 let postsRef = db.collection('posts')
 
-function addMyNewPost(post = { user: "spiderman", texto: "mira que bonita foto", date: Date.now() }) {
-    return postsRef.add(post)
+
+
+//pasar a la funcion el objeto que se encuentra en la base de datos de firebase
+function addMyNewPost(post = { user: "spiderman", texto: "mira que bonita foto", date: Date.now() }) { 
+    return postsRef.add(post) // <--- esto es una promesa
 }
 
-// 
+// leer la coleccion de post
 postsRef.onSnapshot(snap => {
     let p = document.querySelector('#posts')
     p.innerHTML = ''
     snap.forEach(doc => {
         let div = `<div>
-            <img src="${doc.data().foto}" />
+            <img src="${doc.data().foto}" /> // doc.data xq ahi esta la data
             <p>${doc.data().texto}</p>
         </div>`
         let nodo = document.createElement('div')
